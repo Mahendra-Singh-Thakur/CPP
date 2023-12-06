de7f3f0f4402c9e2a80a7ed9f7a32e0b4d62e254
Mahendra-Singh-Thakur <thakurmahendrasingh907@email.com>
2023-12-06 19:38:10 +0530

    array

---
+++ If-Else_Loop/Array.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int size;
+    cout << "Enter array size :  ";
+    cin >> size;
+    cout << "Enter array elements : " << endl;
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        cout << "for " << i << " index = ";
+        cin >> arr[i];
+    }
+    cout << "Array is : { ";
+    for (int i = 0; i < size - 1; i++)
+    {
+        cout << arr[i] << " , ";
+    }
+    cout << arr[size - 1] << " }" << endl;
+
+    return 0;
+}
